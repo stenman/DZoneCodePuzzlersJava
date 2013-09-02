77da0c5cf186858089140bda05c855a99e106784
stenman.desktop <roger.stenman@gmail.com>
2013-09-02 22:17:07 +0200

    -Got started with the NQueensPuzzle

--- src/com/example/dzonecodepuzzlers/main/Start.java
+++ src/com/example/dzonecodepuzzlers/main/Start.java
@@ -1,5 +1,7 @@
 package com.example.dzonecodepuzzlers.main;
 
+import java.util.Arrays;
+
 import com.example.dzonecodepuzzlers.puzzles.KPalindrome;
 
 public class Start {
@@ -9,7 +11,34 @@ public class Start {
 		// KPalindrome
 		// KPalindrome kPalindrome = new KPalindrome();
 		// kPalindrome.testKPalindrome();
+
+		// NQueensPuzzle
+		// NQueensPuzzle nQueensPuzzle = new NQueensPuzzle();
+		// nQueensPuzzle.testNQueensPuzzle();
 		
+		
+		nQueen();
+		
+	}
+	
+	public static void nQueen(){
+		//Skapa ett bräde (array) på 8x8 rutor. 1-8, a-h
+		String[][] board = new String[8][8];
+		int k = 0;
+		
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				k++;
+				System.out.println("k="+k+" "+board[j]);
+			}
+		}
+		
+		
+		//"placera" ut en drottning på en ruta
+		
+		//räkna ut vilka rutor som nu är lediga/upptagna
+		
+		//repetera
 		
 	}
 }
--- src/com/example/dzonecodepuzzlers/puzzles/KPalindrome.java
+++ src/com/example/dzonecodepuzzlers/puzzles/KPalindrome.java
@@ -1,8 +1,18 @@
 package com.example.dzonecodepuzzlers.puzzles;
 
+//Thursday Code Puzzler:K Palindromes
+//08.29.2013   
+//It's Thursday, so it's time for another code puzzler. The idea is simple: solve the coding problem as efficiently as you can, in any language or framework that you find suitable.
+//
+//K Palindromes
+//
+//You probably already know what a palindrome is: a string to results in the same word, whether read left to right, or right to left. A K Palindrome is when a string can be tranformed into a palindrome by changing K characters at most. Regular palindromes have K=0. 
+//
+//Your challenge today is to write a method which takes a string and a value for k and returns true if it the string qualifies as a K palindrome. 
+
 public class KPalindrome {
-	
-	public void testKPalindrome(){
+
+	public void testKPalindrome() {
 		System.out.println("lobor (expect true): " + kPalindrome("lobor", 1));
 		System.out.println("lobor (expect false): " + kPalindrome("lobor", 0));
 		System.out.println("lobor (expect false): " + kPalindrome("lobor", 2));
@@ -11,14 +21,15 @@ public class KPalindrome {
 		System.out.println("lobbor (expect false): " + kPalindrome("lobbor", 2));
 
 	}
-	
-	public boolean kPalindrome(String str, int k){
-		for (int i = 0; i < str.length()/2; i++) {
-			if(str.toLowerCase().substring(i, i+1).compareTo(str.toLowerCase().substring(str.length()-(i+1), str.length()-i)) != 0){
+
+	public boolean kPalindrome(String str, int k) {
+		for (int i = 0; i < str.length() / 2; i++) {
+			if (str.toLowerCase().substring(i, i + 1)
+					.compareTo(str.toLowerCase().substring(str.length() - (i + 1), str.length() - i)) != 0) {
 				k--;
 			}
 		}
-		if(k==0){
+		if (k == 0) {
 			return true;
 		}
 		return false;
